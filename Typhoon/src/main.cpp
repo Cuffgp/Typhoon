97638f61ad9ca2f5ab92fa7999e483c9fce71e12
Keith Cuff <cuffgp@gmail.com>
2024-12-03 12:41:51 -0700

    Start of project

---
+++ Typhoon/src/main.cpp
@@ -0,0 +1,8 @@
+#include "typch.h"
+
+#include <iostream>
+
+int main ()
+{
+	std::cout << "Hello World" << std::endl;
+}
---
+++ premake5.lua
@@ -0,0 +1,76 @@
+workspace "Typhoon"
+	architecture "x64"
+	startproject "Typhoon"
+
+	configurations 
+	{
+		"Debug",
+		"Release"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+VULKAN_SDK = os.getenv("VULKAN_SDK")
+
+Library = {}
+Library["Vulkan"] = "%{VULKAN_SDK}/Lib/vulkan-1.lib"
+
+IncludeDir = {}
+IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
+
+project "Typhoon"
+	location "Typhoon"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	pchheader "typch.h"
+	pchsource "Typhoon/src/typch.cpp"
+
+	files 
+	{ 
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.c",
+		"%{prj.name}/src/**.hpp", 
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{IncludeDir.VulkanSDK}"
+	}
+	
+	links
+	{
+		"%{Library.Vulkan}",
+	}
+	
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		runtime "Debug"
+		symbols "On"
+		
+		links
+		{
+
+		}
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		runtime "Release"
+		optimize "On"
+		
+		links
+		{
+
+		}
